f471274cf5342c088fde72a29f952b3a564ef3f4
jasmineris <133705112+jasmineris@users.noreply.github.com>
2023-05-15 21:52:36 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,38 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
+
+choice = int(input("Введите номер операции: "))
+
+result = 0
+
+if choice == 1:
+    result = add(num1, num2)
+elif choice == 2:
+    result = subtract(num1, num2)
+elif choice == 3:
+    result = multiply(num1, num2)
+elif choice == 4:
+    result = divide(num1, num2)
+else:
+    print("Некорректный выбор операции!")
+
+print(f"Результат: {result}")
+
